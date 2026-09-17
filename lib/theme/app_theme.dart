611531ae9fef319a7667809a75aed085ba05a4fd
Barmanvivek0786 <barmanvivek0786@gmail.com>
2026-09-17 13:36:20 +0530

    Update textStyle to use GoogleFonts.plusJakartaSans

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -189,7 +189,7 @@ class AppTheme {
     ),
     inputDecorationTheme: InputDecorationThemeData(
       filled: true,
-      textStyle: GoogleFonts.plusJakartaSans(
+     GoogleFonts.plusJakartaSans(
         fontSize: 14,
         color: textPrimary,
       ),
@@ -372,7 +372,7 @@ class AppTheme {
     ),
     inputDecorationTheme: InputDecorationThemeData(
       filled: true,
-      textStyle: GoogleFonts.plusJakartaSans(
+     GoogleFonts.plusJakartaSans(
         fontSize: 14,
         color: Colors.white,
       ),
