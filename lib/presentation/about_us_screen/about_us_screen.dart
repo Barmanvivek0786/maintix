39e6289131997178142f77a356c807aaa1fcd336
Barmanvivek0786 <barmanvivek0786@gmail.com>
2026-09-19 16:36:39 +0530

    Fix import formatting in about_us_screen.dart

--- lib/presentation/about_us_screen/about_us_screen.dart
+++ lib/presentation/about_us_screen/about_us_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:google_fonts/google_fonts.dart'; 
 import '../../theme/app_theme.dart';
 
 class AboutUsScreen extends StatelessWidget {
