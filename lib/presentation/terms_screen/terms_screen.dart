058922d7855e38f7ad58911ab61dac62f612a85f
Vivek barman <barmanvivek0786@gmail.com>
2026-09-25 00:55:40 +0530

    fix: remove invalid const from terms screen gradient (theme-aware primaryNavy)

--- lib/presentation/terms_screen/terms_screen.dart
+++ lib/presentation/terms_screen/terms_screen.dart
@@ -41,7 +41,7 @@ class TermsScreen extends StatelessWidget {
                   width: double.infinity,
                   padding: const EdgeInsets.all(20),
                   decoration: BoxDecoration(
-                    gradient: const LinearGradient(
+                    gradient: LinearGradient(
                       colors: [AppTheme.primaryNavy, Color(0xFF1A3F5C)],
                       begin: Alignment.topLeft,
                       end: Alignment.bottomRight,
